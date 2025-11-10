38597c9a68a44f7b165f26ec621745dd30d4ef2b
lordxsumit <sum56it@gmail.com>
2025-11-10 18:30:37 +0530

    added frontend , backend and logic

--- app.js
+++ app.js
@@ -1,35 +1,68 @@
-const express = require('express')
-const path = require("path")
-const socket = require('socket.io')
-const http = require('http')
-const {Chess} = require('chess.js')
-const { title } = require('process')
+const express = require("express");
+const path = require("path");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const { title } = require("process");
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-const io = socket(server)
+const io = socket(server);
 
-const chess = new Chess()
-let players = {}
-let currentPlayer = "W"
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
 
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index", {title: "Chess Game"})
-})
+  res.render("index", { title: "Chess Game" });
+});
 
-io.on("connection", function(uniqueScoket){
-    console.log("Connected");
+io.on("connection", function (uniqueSocket) {
+  console.log("Connected");
 
-    uniqueScoket.on("churan", function(){
-        // console.log("churan received");
-        io.emit("churan paapdi")
-    })
-})
+  if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emit("playerRole", "b");
+  } else {
+    uniqueSocket.emit("spectatorRole");
+  }
 
-server.listen(3000, function(){
-    console.log("listening on port 3000");
-})
+  uniqueSocket.on("disconnect", function () {
+    if (uniqueSocket.id === players.white) {
+      delete players.white;
+    } else if (uniqueSocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniqueSocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniqueSocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniqueSocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move: ", move);
+        uniqueSocket.emit("invalidMove", move);
+      }
+    } catch (error) {
+      console.log(error);
+      uniqueSocket.emit("Invalid move: ", move);
+    }
+  });
+});
+
+server.listen(3000, function () {
+  console.log("listening on port 3000");
+});
